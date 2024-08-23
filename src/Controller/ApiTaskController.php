6d3ec0c65a9ead2308eb3a664a109ae9f13af0a7
mey <meydetour@gmail.com>
2024-08-23 19:51:12 +0200

    debug

--- src/Controller/ApiTaskController.php
+++ src/Controller/ApiTaskController.php
@@ -345,7 +345,7 @@ class ApiTaskController extends AbstractController
                         ]
                         , Response::HTTP_UNPROCESSABLE_ENTITY);
                 }
-
+dd($task,$data['order']);
                 $this->reorderTask($task->getProject(), $task->getCol(), $task, $data['order']);
 
 
