90b7a418cc18cfffc2c108bc241ae2b182ba54cb
mey <meydetour@gmail.com>
2024-08-23 15:50:47 +0200

    debug

--- src/Controller/ApiTaskController.php
+++ src/Controller/ApiTaskController.php
@@ -404,7 +404,7 @@ class ApiTaskController extends AbstractController
     {
         try {
             $tasks = $this->taskRepository->findBy(['project' => $project, 'col' => $col], ['taskOrder' => 'ASC'])->toArray();
-
+            dd($tasks);
             foreach ($tasks as $key => $task) {
                 $task->setTaskOrder($key);
                 $this->entityManager->persist($task);
